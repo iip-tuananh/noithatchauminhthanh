f470c6d4e17c03ada79631b08d3a41e072ee6089
thang197 <thangnguyennvt1410@gmail.com>
2025-05-28 06:52:58 +0700

    F

--- resources/views/site/layouts/master.blade.php
+++ resources/views/site/layouts/master.blade.php
@@ -45,20 +45,22 @@
 
             <ul class="mobile-nav__contact list-unstyled">
                 <li>
-                    <i class="fa fa-envelope"></i>
-                    <a href="mailto:needhelp@packageName__.com">needhelp@ambed.com</a>
+                    <i class="fa fa-phone-alt"></i>
+                    <a href="tel:{{ $config->hotline }}">{{ $config->hotline }}</a>
                 </li>
                 <li>
-                    <i class="fa fa-phone-alt"></i>
-                    <a href="tel:666-888-0000">666 888 0000</a>
+                    <i class="fa fa-map-marker"></i>
+                    <a href="#">{{ $config->address_company }}</a>
                 </li>
+
             </ul><!-- /.mobile-nav__contact -->
             <div class="mobile-nav__top">
                 <div class="mobile-nav__social">
-                    <a href="#" class="fab fa-twitter"></a>
-                    <a href="#" class="fab fa-facebook-square"></a>
-                    <a href="#" class="fab fa-pinterest-p"></a>
-                    <a href="#" class="fab fa-instagram"></a>
+                    <a href="{{$config->facebook}}" target="_blank"><i class="fab fa-facebook"></i></a>
+
+                    <a href="{{$config->instagram}}" target="_blank"><i class="fab fa-tiktok"></i></a>
+
+                    <a href="{{$config->youtube}}" target="_blank"><i class="fab fa-youtube"></i></a>
                 </div><!-- /.mobile-nav__social -->
             </div><!-- /.mobile-nav__top -->
 
